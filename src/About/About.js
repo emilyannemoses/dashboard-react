c596094f8740a447a1ac9e9e866defaeb70750bb
eamoses <e.a.moses@gmail.com>
2019-06-22 18:44:59 -0500

    add other dashboard to about page

--- src/About/About.js
+++ src/About/About.js
@@ -13,20 +13,28 @@ class About extends Component {
                 </div>
                 <div>
                     <p>
-                        I've been a professional software developer for four years and counting. I've worked on enterprise software in Python, Go and JavaScript.  I was first introduced to React in 2016, and proceeded to use it to create a form component on an already functioning web application. 
-                        Since then I've worked with AngularJS, RiotJS, VueJS and LoneJS.  All frameworks are more or less the same, but in picking up where I left off with React I'm impressed by how far it has come!
-                        I decided to use React Router 4 for this project, and I'm really happy with the performance.
+                        I've been a professional software developer for four years and counting. I've worked on enterprise software in Python, Go and JavaScript.  
+                        I was first introduced to React in 2016, and proceeded to use it to create a form component on an already functioning web application. 
+                        Since then I've worked with AngularJS, RiotJS, VueJS and LoneJS.  
+                    </p>
+                    <p>
+                        All frameworks are more or less the same, but in picking up where I left off with React I'm impressed by how far it has come!
+                        I decided to use React Router 4 for this project, and I'm really happy with the performance. I'm also a <i>widget dashboard enthusiast</i> and I've <a href="https://eamoses.github.io/widget-dashboard/" target="_blank">made one before based on the Momentum Dash design.</a>
                     </p>
                 </div>
                 <div>
                     <p>
-                        I am not a designer.  Fortunately people with beautiful design chops exist, and for this project I borrowed Julie Park's <a href="https://codepen.io/juliepark/pen/KLWmxO" target="_blank">CodePen</a> and build functionality into it!
+                        I am not a designer.  Fortunately, people with beautiful design chops exist, and for this project I borrowed Julie Park's <a href="https://codepen.io/juliepark/pen/KLWmxO" target="_blank">CodePen</a> and build functionality into it!
                     </p>
                 </div>
                 <div>
                     <p>
-                        The widget dashboard first saves the user's name to the user's local storage. This can be deleted and reset in the Settings component. The widget uses a weather API through the browser to find the users location and current weather. It also displays the current time and date.
-                        The user has the option to link a Google Calendar if they wish. The To-do list saves the items to local storage, and these items can be checked off or deleted.
+                        The widget dashboard first saves the user's name to the user's local storage. This can be deleted and reset in the Settings component. 
+                        The widget uses a weather API through the browser to find the users location and current weather. It also displays the current time and date.
+                        The user has the option to link a Google Calendar if they wish. 
+                    </p>
+                    <p>
+                        The To-do list saves the items to local storage, and these items can be checked off or deleted.
                         Implementation of the Spotify API was the most complex. I hadn't worked with this API since 2016 and it has changed drastically!  To log in, go to the Settings tab and log in with your Spotify login credentials. It will then load and play your most current playlist.
                     </p>
                 </div>
