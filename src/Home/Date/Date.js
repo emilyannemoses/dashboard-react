4af995d8116d430855a2918efcdcb44af48f8018
eamoses <e.a.moses@gmail.com>
2019-06-23 14:54:57 -0500

    css grid is set up for all components

--- src/Home/Date/Date.js
+++ src/Home/Date/Date.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './Date.css'
 
 function Date() {
     return (
-        <div>This is the DATE component</div>
+        <div>
+            <h1 id="date"></h1>
+            <a id="linkToCalendar" href="" target="_blank"></a>
+            <p><a href="">go to settings to edit calendar</a></p>
+        </div>
     )
 }
 
--- src/Home/Todo/Todo.js
+++ src/Home/Todo/Todo.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import './Todo.css'
 
 function Todo() {
     return (
-        <div>This is the TODO component</div>
+        <div>
+            <div className="reminders">
+                <p>reminders</p>
+                <div className="reminder--btns" onclick="displayAddItem()">
+                <i className="fas fa-ellipsis-h"></i>
+                </div>
+            </div>
+            <div className="reminders--list">
+                <ul id="listOfItemsID" className="things">
+                </ul>
+            </div>
+            <form id="formToAddItemsID" className="addItems" style={{display: 'none'}}>
+                <input type="text" name="item" placeholder="What do you need to do today?" required />
+                <input type="submit" value="Add Item" />
+            </form>
+        </div>
     )
 }
 
