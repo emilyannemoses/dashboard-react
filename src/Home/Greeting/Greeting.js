be57af4a861ff477a9ea82368ee086f7754135a1
eamoses <e.a.moses@gmail.com>
2019-06-23 14:08:37 -0500

    greeting component functional

--- src/Home/Greeting/Greeting.js
+++ src/Home/Greeting/Greeting.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Greeting.css';
 
-function Greeting() {
-    return (
-        <div>
-            <div className="user">
-                <div id="greetingContainer">
-                    <div className="text">Please enter your full name</div>
-                    <div id="yourName"><input /></div>
+class Greeting extends Component {
+    render() {
+        let lsName = localStorage.getItem('names').replace(/\"/g, "");
+        return (
+            <div>
+                <div className="user">
+                    { (lsName.length > 0)  ? (
+                        <div id="greetingDisplay" className="greeting">{lsName}</div>
+                    ) : (
+                        <div id="greetingContainer">
+                            <div className="text">Please enter your full name</div>
+                            <div className="name"><input id="name" value={this.props.nameVal} onKeyPress={this.props.poopFunc} onChange={char => this.props.setNameState(char)} /></div>
+                        </div>
+                    ) }
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Greeting;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,17 +4,17 @@ import Weather from './Weather/Weather';
 import Music from './Music/Music';
 import Date from './Date/Date'
 import Todo from './Todo/Todo';
+import Time from './Time/Time';
 
 class Home extends Component {
 
     render() {
+        let lsName = localStorage.getItem('names').replace(/\"/g, "");
         return (
             <div className="main-grid">
                 <div className="block one">
-                    <div id="greetingDisplayFirst" className="timeGreeting"></div>
-                    {/* <div id="time" className="time"></div>
-                    <div id="greeting" className="timeGreeting"></div>
-                    */}
+                    <Time />
+                    <p>{lsName}</p>
                 </div>
   
                 <div className="block two">
