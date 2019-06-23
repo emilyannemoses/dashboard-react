65b3e69906b7e165033c3af3246a5158197e36f4
Emily Anne Moses <e.a.moses@gmail.com>
2019-06-23 14:55:29 -0500

    css grid is set up for all components (#6)

--- src/Home/Weather/Weather.js
+++ src/Home/Weather/Weather.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './Weather.css';
 
 function Weather() {
     return (
-        <div>This is the WEATHER component</div>
+        <div>
+            <div id="weatherWidget" className="weatherWidget">93</div>
+            <div id="description" className="description">Clear</div>
+            <div id="homeCity" className="homeCity">Austin</div>
+        </div>
     )
 }
 
