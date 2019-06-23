82ba8667e9f4087d36c9add468d95c012baead0c
eamoses <e.a.moses@gmail.com>
2019-06-22 19:23:30 -0500

    fixes contact link and makes form plain for styling later

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,11 +6,28 @@ class Contact extends Component {
     render() {
         return (
             <div className="contact">
-                <div className="contact-header">Contact me</div>
+                <div className="contact-header">
+                    Contact me
+                </div>
                 <div className="contact-form">
-                    <p>
-                        Put contact form here
-                    </p>
+                    <div id="contact" className="centered">
+                        <div className="formContainer">
+                            <form method="POST" action="https://formspree.io/eamoses@gmail.com">
+                                <fieldset>
+                                    <label htmlFor="name">NAME</label>
+                                    <input type="text" id="name" name="user_name" />
+                                    <label htmlFor="email">EMAIL</label>
+                                    <input type="email" id="mail" name="user_email" />
+                                </fieldset>
+                                <fieldset>
+                                    <label htmlFor="bio">MESSAGE</label>
+                                    <textarea id="bio" name="user_bio"></textarea>
+                                </fieldset>
+                                <button type="submit" className="submit">SEND</button>
+                                <button type="reset" className="reset">RESET</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
