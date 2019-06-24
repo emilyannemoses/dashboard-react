ca8e073221ee5050794bbd9cb764d9acf1940058
eamoses <e.a.moses@gmail.com>
2019-06-23 20:44:11 -0500

    current date comes in dynamically

--- src/Home/TodaysDate/TodaysDate.js
+++ src/Home/TodaysDate/TodaysDate.js
@@ -2,20 +2,24 @@ import React, {Component} from 'react';
 import './TodaysDate.css'
 import {Link} from 'react-router-dom';
 
-class TodaysDate extends Component() {
+class TodaysDate extends Component {
     getDate = () => {
-         let today = new Date();
-         let month = today.getMonth();
-         let day = today.getUTCDate();
-         let year = today.getFullYear();
-         let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        return {month, day, year, months}
+        let today = new Date();
+        let month = today.getMonth();
+        let day = today.getDay();
+        let numberDay = today.getDate();
+        let year = today.getFullYear();
+        let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let dayOfWeek = weekDays[day]
+         return {month, numberDay, year, months, dayOfWeek}
     }
     render() {
-        const {month, day, year, months} = this.getDate();
+        const {month, numberDay, year, months, dayOfWeek} = this.getDate();
         return (
             <div>
-                <h1 id="date">{months[month]} {day}  {year}</h1>
+                <h1 id="date">{dayOfWeek}</h1>
+                <h1 id="date">{months[month]} {numberDay},  {year}</h1>
                 <p><Link to="/settings">go to settings to edit calendar</Link></p>
             </div>
         )
