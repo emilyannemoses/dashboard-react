8b2607cb69b4c72c9b2e21e12b580b3de2f6e888
Emily Anne Moses <e.a.moses@gmail.com>
2019-06-23 20:44:46 -0500

    Date component (#9)
    * rename date component to todaysdate
    
    * current date comes in dynamically

---
+++ src/Home/TodaysDate/TodaysDate.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './TodaysDate.css'
+import {Link} from 'react-router-dom';
+
+class TodaysDate extends Component {
+    getDate = () => {
+        let today = new Date();
+        let month = today.getMonth();
+        let day = today.getDay();
+        let numberDay = today.getDate();
+        let year = today.getFullYear();
+        let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let dayOfWeek = weekDays[day]
+         return {month, numberDay, year, months, dayOfWeek}
+    }
+    render() {
+        const {month, numberDay, year, months, dayOfWeek} = this.getDate();
+        return (
+            <div>
+                <h1 id="date">{dayOfWeek}</h1>
+                <h1 id="date">{months[month]} {numberDay},  {year}</h1>
+                <p><Link to="/settings">go to settings to edit calendar</Link></p>
+            </div>
+        )
+    }
+}
+
+export default TodaysDate;
