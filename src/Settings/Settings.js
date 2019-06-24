4ee28612c6e9718ad28cd9753e85dc0a76efa70d
eamoses <e.a.moses@gmail.com>
2019-06-24 18:57:14 -0500

    name settings complete

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -2,10 +2,34 @@ import React, {Component} from 'react';
 import './Settings.css';
 
 class Settings extends Component {
-
+    reset() {
+        localStorage.removeItem("names");
+    }
     render() {
+        let lsName;
+        if (localStorage.names) {
+            lsName = localStorage.getItem('names').replace(/"/g,"");
+        }
         return (
-            <div className="settings">This is the Settings component</div>
+            <div className="settings-grid">
+                <div className="block one">
+                    <div className="name-heading">Change display name</div>
+                    <br></br>
+                    { localStorage.names ? (
+                        <span className="name-change">{lsName} <span className="remove-name" onClick={this.reset}>&nbsp;&nbsp;&#8594;&nbsp;&nbsp;reset</span></span>
+                    ) : (
+                        <div className="name-change remove-name">&#8592; Enter your name in the space provided on the Menu.</div>
+                    )}
+                </div>
+
+                <div className="block two">
+                    block two
+                </div>
+
+                <div className="block three">
+                    Log in to Spotify
+                </div>
+            </div>
         )
     }
 }
