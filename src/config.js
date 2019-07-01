ff022ab0219afe69c96f2a36ddc4c9ff08459852
eamoses <e.a.moses@gmail.com>
2019-07-01 18:20:36 -0500

    Remove

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = "279710054a5e456ca6a80d4561e56868";
-export const redirectUri = "https://widget-dashboard-app.appspot.com/settings/"; // http://localhost:3000/settings/  // http://localhost:3000/settings/  //https://eamoses.github.io/settings/
-export const scopes = [
-    "user-read-private",
-    "user-read-email",
-    "streaming", 
-    "user-read-birthdate"
-];
