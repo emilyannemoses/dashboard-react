446fe46986b997e605951ad5a3f9b8164213a18a
eamoses <e.a.moses@gmail.com>
2019-06-30 19:15:20 -0500

    fixes url redirect

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "279710054a5e456ca6a80d4561e56868";
-export const redirectUri = "http://localhost:3000/settings/"; // https://eamoses.github.io/settings
+export const redirectUri = "https://eamoses.github.io/settings/"; // http://localhost:3000/settings/
 export const scopes = [
     "user-read-private",
     "user-read-email",
